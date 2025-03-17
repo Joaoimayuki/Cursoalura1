9be612951359ddbd58151703aec752739ba9d2e4
João Imayuki <142546850+Joaoimayuki@users.noreply.github.com>
2025-03-17 08:38:05 -0300

    segundo

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
+   const alternaContraste = document.getElementById('alterna-contraste')
+   alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"></script>
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
